704ecadb0af89858d7525c0c15a354b0f5de05d2
mikejeffers <mike.s.jeffers@gmail.com>
2024-01-19 23:41:34 -0500

    basic user register and login routes with jwt and response structure to match other services

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"github.com/jmoiron/sqlx"
 	_ "github.com/lib/pq"
+	"github.com/redis/go-redis/v9"
 )
 
 var (
@@ -16,6 +19,11 @@ var (
 	DB_NAME     = os.Getenv("POSTGRES_DB")
 	DB_HOST     = os.Getenv("POSTGRES_HOST")
 	DB_PORT     = os.Getenv("POSTGRES_PORT")
+
+	REDIS_PORT     = os.Getenv("REDIS_PORT")
+	REDIS_PASSWORD = os.Getenv("REDIS_PASSWORD")
+
+	SECRET_JWT = os.Getenv("SECRET_JWT")
 )
 
 func dbConnect() *sqlx.DB {
@@ -29,6 +37,15 @@ func dbConnect() *sqlx.DB {
 	return db
 }
 
+func redisClient() *redis.Client {
+	return redis.NewClient(&redis.Options{
+		Addr:     fmt.Sprintf("localhost:%v", REDIS_PORT),
+		Password: REDIS_PASSWORD,
+		DB:       0,
+		Protocol: 3,
+	})
+}
+
 type UserAuthBody struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
@@ -40,9 +57,51 @@ type UserRow struct {
 	Password string `json:"password" db:"password"`
 }
 
+type UserTokenData struct {
+	Id       int64  `json:"id" db:"id"`
+	Username string `json:"username" db:"username"`
+}
+
+type ReturnedUserData struct {
+	Token string        `json:"token"`
+	User  UserTokenData `json:"user"`
+}
+
+type MyCustomClaims struct {
+	Id       int64  `json:"id"`
+	Username string `json:"username"`
+	jwt.StandardClaims
+}
+
+func signToken(user UserRow) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, MyCustomClaims{
+		Id:       user.Id,
+		Username: user.Username,
+	})
+	return token.SignedString([]byte(SECRET_JWT))
+}
+
+func storeToken(token string, userId int64, redisClient *redis.Client, c *gin.Context) *redis.StatusCmd {
+	return redisClient.SetEx(c, fmt.Sprintf("jwt:%v", userId), token, time.Hour*1)
+}
+
+func storeAndRespond(user UserRow, redisClient *redis.Client, c *gin.Context) (ReturnedUserData, error) {
+	signed, err := signToken(user)
+	if err != nil {
+		return ReturnedUserData{}, err
+	}
+	status := storeToken(signed, user.Id, redisClient, c)
+	if status.Err() != nil {
+		return ReturnedUserData{}, status.Err()
+	}
+	data := ReturnedUserData{Token: signed, User: UserTokenData{user.Id, user.Username}}
+	return data, nil
+}
+
 func setupRouter() *gin.Engine {
 	r := gin.Default()
 	db := dbConnect()
+	red := redisClient()
 
 	r.POST("/login", func(c *gin.Context) {
 		var requestBody UserAuthBody
@@ -56,7 +115,12 @@ func setupRouter() *gin.Engine {
 				c.JSON(400, gin.H{"message": "no such user"})
 				return
 			}
-			c.JSON(200, users[0])
+			data, err := storeAndRespond(users[0], red, c)
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to generate token"})
+				return
+			}
+			c.JSON(200, data)
 		}
 	})
 
@@ -72,7 +136,12 @@ func setupRouter() *gin.Engine {
 				c.JSON(400, gin.H{"message": "unable to add user"})
 				return
 			}
-			c.JSON(200, users[0])
+			data, err := storeAndRespond(users[0], red, c)
+			if err != nil {
+				c.JSON(500, gin.H{"message": "Failed to generate token"})
+				return
+			}
+			c.JSON(200, data)
 		}
 	})
 
